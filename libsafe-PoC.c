356ddf50afacdb6b40c91b62422ee756ff696264
David Tagatac <dtagatac@cs.columbia.edu>
2015-02-22 14:48:03 -0500

    revised results section + conclusion

--- libsafe-PoC.c
+++ libsafe-PoC.c
@@ -10,7 +10,7 @@ void *func1(void *none)
 	{
 		if(!ok)
 			continue;
-		strcpy(buf, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		strcpy(buf, "AAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAA");
 		break;
 	}
 	puts("func1 overflow!");
@@ -20,19 +20,19 @@ void *func2(void *none)
 {
 	char buf[8];
 	ok = 1;
-	strcpy(buf, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	strcpy(buf, "AAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAA");
 	puts("func2 overflow!!");
 }
 
 int main()
 {
 	pthread_t t1, t2;
-	
+
 	pthread_create(&t1, NULL, &func1, NULL);
 	pthread_create(&t2, NULL, &func2, NULL);
-	
+
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
-	
+
 	return 0;
 }
